12c5defd55602b90959009d31864c6152340e3aa
JacobHeslop <74215508+JacobHeslop@users.noreply.github.com>
2022-02-08 23:52:54 -0700

    Changed functions of scripts/main.js
    Removed almost all scripts from overlay.html and moved them to main.js.
    Added sound files to have some sort of action while still not connected to bots. Unable to get sound files to work. Removed functions.

--- UI-draft-restart/scripts/main.js
+++ UI-draft-restart/scripts/main.js
@@ -1,57 +1,29 @@
-/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
- (function() {
+(function() {
 
-  let currentButton;
-
-  function loadWorkspace(button) {
-    const workspace = Blockly.getMainWorkspace();
-    if (button.blocklySave) {
-      Blockly.serialization.workspaces.load(button.blocklySave, workspace);
-    }
-  }
-
-  function save(button) {
-    button.blocklySave = Blockly.serialization.workspaces.save(
-        Blockly.getMainWorkspace());
-  }
-
-  function handleStart() {
-    // ros code goes to send bot action
-    save(currentButton);
-  }
-
-  function enableEditMode() {
-    document.body.setAttribute('mode', 'edit');
-    document.querySelectorAll('.button').forEach(btn => {
-      btn.removeEventListener('click', handlePlay);
-      btn.addEventListener('click', enableBlocklyMode);
-    });
-  }
-
-  function enableMakerMode() {
-    document.body.setAttribute('mode', 'maker');
-    document.querySelectorAll('.button').forEach(btn => {
-      btn.addEventListener('click', handlePlay);
-      btn.removeEventListener('click', enableBlocklyMode);
-    });
-  }
-
-  function enableBlocklyMode(e) {
-    document.body.setAttribute('mode', 'blockly');
-    currentButton = e.target;
-    loadWorkspace(currentButton);
-  }
-
-  document.querySelector('#edit').addEventListener('click', enableBlocklyMode);
-  document.querySelector('#done').addEventListener('click', enableMakerMode);
-  document.querySelector('#save').addEventListener('click', handleSave);
-
-  enableMakerMode();
+var blocklyArea = document.getElementById('blocklyArea');
+var blocklyDiv = document.getElementById('blocklyDiv');
+var demoWorkspace = Blockly.inject(blocklyDiv,
+    {media: '/media/',
+     toolbox: document.getElementById('toolbox')});
+var onresize = function(e) {
+  // Compute the absolute coordinates and dimensions of blocklyArea.
+  var element = blocklyArea;
+  var x = 0;
+  var y = 0;
+  do {
+    x += element.offsetLeft;
+    y += element.offsetTop;
+    element = element.offsetParent;
+  } while (element);
+  // Position blocklyDiv over blocklyArea.
+  blocklyDiv.style.left = x + 'px';
+  blocklyDiv.style.top = y + 'px';
+  blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+  blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+  Blockly.svgResize(demoWorkspace);
+};
+window.addEventListener('resize', onresize, false);
+onresize();
+Blockly.svgResize(demoWorkspace);
 
 })();
-
-
