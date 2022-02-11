52d5aaee12ec666789e37230031a3cc7a8008bf6
JacobHeslop <74215508+JacobHeslop@users.noreply.github.com>
2022-02-11 15:01:12 -0700

    main functions working
    Moved some event listeners outside of Blockly build workspace. 
    Created logic functions and event listeners for settings, start and stop.
    Stop and settings functions only have alerts; start function has sound.

--- UI-draft-restart/scripts/main.js
+++ UI-draft-restart/scripts/main.js
@@ -1,11 +1,6 @@
 (function() {
-
-// create variables to be used inside functions
-let currentButton;
-
-
 // master function to create blockly page
-function main()
+function buildBlocklyWorkspace()
 {
 var blocklyArea = document.getElementById('blocklyArea');
 var blocklyDiv = document.getElementById('blocklyDiv');
@@ -28,22 +23,106 @@ var onresize = function(e) {
   blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
   blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
   Blockly.svgResize(demoWorkspace);
-};
+  };
+}
+
+
+
+/*
+ * Name: settingsMessage
+ * Algorithm: Ideally a drop down settings menu
+ * Input/Parameters: none
+ * Output: drops down a menu
+ * Notes: currently only shows an alert
+ */
+function settingsMessage()
+{
+  // placeholder to currently verify that the button works
+  alert("You have clicked on settings");
+
+  // call function to show the dropdown content
+
+  // close the dropdown if the user clicks outside of dropdown
+    // create event listener listening for any action outside of dropdown buttons
+
+  // end function
+}
+
+
+
+/*
+ * Name: startButtonLogic
+ * Algorithm: Call functions needed to run Blockly blocks for Duckiebots
+ * Input/Parameters: Blocks have been placed into work area
+ * Output: Calls functions that run Duckiebots.
+ * Notes: currently only shows an alert
+ */
+function startButtonLogic()
+{
+  // placeholder to currently verify that the button works
+  alert( "You clicked start" );
+
+  // initialize function/variables
+  var tiresPealing = new Audio( 'sounds/Tires.m4a' );
+
+  // check that there is blocks in the workspace
+
+    // play sounds
+    tiresPealing.play();
+
+    // call functions that translate those blocks. Maybe in DuckBlock.js file?
+
+  // else assume that no blocks have been placed in the workspace
+
+    // send alert to window that there should be development before the bot will run
+    alert( "You haven't built blocks yet to run, idots" );
+
+  // end function
+}
+
+
+
+/* 
+ * Name: stopButtonLogic
+ * Algorithm: calls functions that stop functions of the Duckiebots
+ *            either stops signal or sends an interupt stop command
+ * Input/Parameters: none
+ * Output: calls functions to stop Duckiebot
+ * Notes: currently only shows an alert
+ */
+function stopButtonLogic()
+{
+  // placeholder to currently verify that the button works
+  alert( "You clicked stop" );
+
+  // initialize function/variables as needed
+
+  // check for current commands being sent to bot
+
+    // end commands being sent to bot
+
+  // send stop signal
+
+  // end function
+}
+
+
+
+// run the main executables to build blockly workspace
+buildBlocklyWorkspace();
+
+
+
+// create aditional listener buttons for settings, start and stop
+document.getElementById( 'settingsMenu' ).addEventListener( 'click', settingsMessage );
+document.getElementById( 'startbutton' ).addEventListener( 'click', startButtonLogic );
+document.getElementById( 'stopbutton' ).addEventListener( 'click', stopButtonLogic );
+
+
+
+// creates listeners to resize the Blockly workspace if the window size changes
 window.addEventListener('resize', onresize, false);
 onresize();
 Blockly.svgResize(demoWorkspace);
-}
-
-function settingsMessage()
-{
-  alert("You have clicked on settings");
-}
-
-// run the main executables to build blockly workspace
-main();
-
-// create aditional listener buttons for settings, start and stop
-document.getElementById('settingsMenu').addEventListener( 'click', settingsMessage );
-
 
 })();
