a4793d6282f04983bceb25dc30214e907de26411
Ari Jaramillo <aej88@nau.edu>
2022-02-26 21:24:44 -0700

    moving folders
    remove UI_draft/restart folder

---
+++ Blockly-UI/customBlocks/moveBot.js
@@ -0,0 +1,27 @@
+Blockly.Blocks['movebot'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Power percentage to:")
+        .appendField("left wheel")
+        .appendField(new Blockly.FieldNumber(0, 0, 100), "leftWheel")
+        .appendField("and right wheel")
+        .appendField(new Blockly.FieldNumber(0, 0, 100), "rightWheel");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(172);
+ this.setTooltip("Block to provide power to each wheel");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['movebot'] = function(block) {
+  var number_leftwheel = block.getFieldValue('leftWheel');
+  var number_rightwheel = block.getFieldValue('rightWheel');
+  // print out variables grabbed for debugging
+  var code = number_leftwheel + ' ' + number_rightwheel;
+  window.alert(code)
+  // can add code here to send vars to bot
+
+  // return the two vars
+  return [code, JavaScript.ORDER_NONE];
+};
