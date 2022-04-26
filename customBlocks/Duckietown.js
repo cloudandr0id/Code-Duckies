eb7f919e73ace1c9219177d6eb4003b004c89a5e
Anthony <als872@nau.edu>
2022-04-26 16:01:21 -0700

    Add untimed moves, stop, and some cleanup

--- customBlocks/Duckietown.js
+++ customBlocks/Duckietown.js
@@ -2,6 +2,8 @@
 Blockly.Blocks['movebot'] = {
     init: function() {
       this.appendDummyInput()
+          .appendField("move bot");
+      this.appendDummyInput()
           .appendField("Linear Velocity:")
           .appendField(new Blockly.FieldNumber(0, -.4, .4), "v")
           .appendField("Angular Velocity:")
@@ -11,7 +13,7 @@ Blockly.Blocks['movebot'] = {
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(60);
-   this.setTooltip("Block to provide power to each wheel");
+   this.setTooltip("Block to move and turn");
    this.setHelpUrl("");
     }
   };
@@ -47,7 +49,7 @@ Blockly.Blocks['movebot'] = {
     }
     `;
 
-    // return the two vars
+    // return the code
     return code;
   };
 
@@ -71,7 +73,7 @@ Blockly.Blocks['moveforward'] = {
   Blockly.JavaScript['moveforward'] = function(block) {
     var v = block.getFieldValue('v');
     var time = block.getFieldValue('time');
-    // TODO: Assemble JavaScript into code variable.
+
     var code =
     `
     var wheel_power = new ROSLIB.Message({
@@ -96,7 +98,7 @@ Blockly.Blocks['moveforward'] = {
     }
     `;
 
-    // return the two vars
+    // return the code
     return code;
   };
 
@@ -120,7 +122,7 @@ Blockly.Blocks['movebackward'] = {
 Blockly.JavaScript['movebackward'] = function(block) {
   var v = block.getFieldValue('v');
   var time = block.getFieldValue('time');
-  // TODO: Assemble JavaScript into code variable.
+
   var code =
   `
     var wheel_power = new ROSLIB.Message({
@@ -145,7 +147,7 @@ Blockly.JavaScript['movebackward'] = function(block) {
     }
     `;
 
-    // return the two vars
+    // return the code
   return code;
 };
 
@@ -169,7 +171,7 @@ Blockly.Blocks['turnleft'] = {
   Blockly.JavaScript['turnleft'] = function(block) {
     var omega = block.getFieldValue('omega');
     var time = block.getFieldValue('time');
-    // TODO: Assemble JavaScript into code variable.
+
     var code =
     `
       var wheel_power = new ROSLIB.Message({
@@ -194,7 +196,7 @@ Blockly.Blocks['turnleft'] = {
       }
       `;
 
-    // return the two vars
+    // return the code
     return code;
   };
 
@@ -218,7 +220,7 @@ Blockly.Blocks['turnright'] = {
   Blockly.JavaScript['turnright'] = function(block) {
     var omega = block.getFieldValue('omega');
     var time = block.getFieldValue('time');
-    // TODO: Assemble JavaScript into code variable.
+
     var code =
     `
       var wheel_power = new ROSLIB.Message({
@@ -243,7 +245,279 @@ Blockly.Blocks['turnright'] = {
       }
       `;
 
-    // return the two vars
+    // return the code
+    return code;
+  };
+
+/////////// MOVE BOT UNTIMED ///////////////////////////////////////////////////
+Blockly.Blocks['movebotuntimed'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("move bot untimed");
+      this.appendDummyInput()
+          .appendField("Linear Velocity:")
+          .appendField(new Blockly.FieldNumber(0, -.4, .4), "v")
+          .appendField("Angular Velocity:")
+          .appendField(new Blockly.FieldNumber(0, -8, 8), "omega");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(60);
+   this.setTooltip("Block to move and turn untimed. Equivalent to movebot with time 0");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blockly.JavaScript['movebotuntimed'] = function(block) {
+    // Get vars
+    var v = block.getFieldValue('v');
+    var omega = block.getFieldValue('omega');
+
+    // Template code to send to bot
+    var code =
+    `
+    var wheel_power = new ROSLIB.Message({
+      header : {
+        stamp : {
+          sec : 0,
+          nanosec : 0
+        }
+      },
+      v: ` + v + `,
+      omega: ` + omega + `
+    });
+
+    if (!isCanceled[myIdx])
+    {
+      cmdVel.publish(wheel_power);
+    }
+    else
+    {
+      return;
+    }
+    `;
+
+    // return the code
+    return code;
+  };
+
+///////// MOVE FORWARD UNTIMED ////////////////////////////////////////////////////
+Blockly.Blocks['moveforwarduntimed'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("move forward untimed");
+      this.appendDummyInput()
+          .appendField("Forward Velocity:")
+          .appendField(new Blockly.FieldNumber(0, 0, .4), "v");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(60);
+   this.setTooltip("Block to move bot forward untimed. Equivalent to move forward with time of 0");
+   this.setHelpUrl("");
+    }
+  };
+  Blockly.JavaScript['moveforward'] = function(block) {
+    var v = block.getFieldValue('v');
+
+    var code =
+    `
+    var wheel_power = new ROSLIB.Message({
+      header : {
+        stamp : {
+          sec : 0,
+          nanosec : 0
+        }
+      },
+      v: ` + v + `,
+      omega: 0
+    });
+
+    if (!isCanceled[myIdx])
+    {
+      cmdVel.publish(wheel_power);
+    }
+    else
+    {
+      return;
+    }
+    `;
+
+    // return the code
+    return code;
+  };
+
+////////// MOVE BACKWARD UNTIMED //////////////////////////////////////////////////
+Blockly.Blocks['movebackwarduntimed'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("move backward untimed");
+    this.appendDummyInput()
+        .appendField("Backward velocity:")
+        .appendField(new Blockly.FieldNumber(0, 0, .4), "v");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+ this.setTooltip("Block to move bot backward untimed. Equivalent to move backward with time 0.");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['movebackward'] = function(block) {
+  var v = block.getFieldValue('v');
+
+  var code =
+  `
+    var wheel_power = new ROSLIB.Message({
+    header : {
+      stamp : {
+        sec : 0,
+        nanosec : 0
+      }
+    },
+    v : ` + -v + `,
+    omega : 0
+    });
+
+    if (!isCanceled[myIdx])
+    {
+      cmdVel.publish(wheel_power);
+    }
+    else
+    {
+      return;
+    }
+    `;
+
+    // return the code
+  return code;
+};
+
+///////// TURN LEFT UNTIMED ///////////////////////////////////////////////////////
+Blockly.Blocks['turnleftuntimed'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("turn left untimed");
+      this.appendDummyInput()
+          .appendField("Angular Velocity:")
+          .appendField(new Blockly.FieldNumber(0, 0, 8), "omega");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(60);
+   this.setTooltip("Block to turn left untimed. Equivalent to turn left with time 0.");
+   this.setHelpUrl("");
+    }
+  };
+  Blockly.JavaScript['turnleft'] = function(block) {
+    var omega = block.getFieldValue('omega');
+
+    var code =
+    `
+      var wheel_power = new ROSLIB.Message({
+      header : {
+        stamp : {
+          sec : 0,
+          nanosec : 0
+        }
+      },
+      v : 0,
+      omega : ` + omega + `
+      });
+
+      if (!isCanceled[myIdx])
+      {
+        cmdVel.publish(wheel_power);
+      }
+      else
+      {
+        return;
+      }
+      `;
+
+    // return the code
+    return code;
+  };
+
+///////// TURN RIGHT UNTIMED //////////////////////////////////////////////////////
+Blockly.Blocks['turnright'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("turn right untimed");
+      this.appendDummyInput()
+          .appendField("Angular Velocity:")
+          .appendField(new Blockly.FieldNumber(0, 0, 8), "omega");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(60);
+   this.setTooltip("Block to turn right untimed. Equivalent to turn right with time of 0");
+   this.setHelpUrl("");
+    }
+  };
+  Blockly.JavaScript['turnright'] = function(block) {
+    var omega = block.getFieldValue('omega');
+
+    var code =
+    `
+      var wheel_power = new ROSLIB.Message({
+      header : {
+        stamp : {
+          sec : 0,
+          nanosec : 0
+        }
+      },
+      v : 0,
+      omega : ` + -omega + `
+      });
+
+      if (!isCanceled[myIdx])
+      {
+        cmdVel.publish(wheel_power);
+      }
+      else
+      {
+        return;
+      }
+      `;
+
+    // return the code
+    return code;
+  };
+
+///////// STOP //////////////////////////////////////////////////////
+Blockly.Blocks['stop'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("stop");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(60);
+   this.setTooltip("Block to stop the bot. Equivalent to any move with speed 0.");
+   this.setHelpUrl("");
+    }
+  };
+  Blockly.JavaScript['stop'] = function(block) {
+
+    var code =
+    `
+      var wheel_power = new ROSLIB.Message({
+      header : {
+        stamp : {
+          sec : 0,
+          nanosec : 0
+        }
+      },
+      v : 0,
+      omega : 0
+      });
+
+      if (!isCanceled[myIdx])
+      {
+        cmdVel.publish(wheel_power);
+      }
+      else
+      {
+        return;
+      }
+      `;
+
+    // return the code
     return code;
   };
 
