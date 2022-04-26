34f805eaf27989c0d7c8f14b0ef139fedb5f4a1c
Anthony <als872@nau.edu>
2022-04-26 16:17:51 -0700

    Cleanup and properly add blocks

--- customBlocks/Duckietown.js
+++ customBlocks/Duckietown.js
@@ -436,7 +436,7 @@ Blockly.Blocks['turnleftuntimed'] = {
   };
 
 ///////// TURN RIGHT UNTIMED //////////////////////////////////////////////////////
-Blockly.Blocks['turnright'] = {
+Blockly.Blocks['turnrightuntimed'] = {
     init: function() {
       this.appendDummyInput()
           .appendField("turn right untimed");
@@ -450,7 +450,7 @@ Blockly.Blocks['turnright'] = {
    this.setHelpUrl("");
     }
   };
-  Blockly.JavaScript['turnright'] = function(block) {
+  Blockly.JavaScript['turnrightuntimed'] = function(block) {
     var omega = block.getFieldValue('omega');
 
     var code =
@@ -488,41 +488,40 @@ Blockly.Blocks['stop'] = {
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(60);
-   this.setTooltip("Block to stop the bot. Equivalent to any move with speed 0.");
+   this.setTooltip("Block to stop the bot. Equivalent to any move with velocity and time 0.");
    this.setHelpUrl("");
     }
   };
   Blockly.JavaScript['stop'] = function(block) {
-
     var code =
     `
-      var wheel_power = new ROSLIB.Message({
+    var wheel_power = new ROSLIB.Message({
       header : {
         stamp : {
           sec : 0,
           nanosec : 0
         }
       },
-      v : 0,
-      omega : 0
-      });
+      v: 0,
+      omega: 0
+    });
 
-      if (!isCanceled[myIdx])
-      {
-        cmdVel.publish(wheel_power);
-      }
-      else
-      {
-        return;
-      }
-      `;
+    if (!isCanceled[myIdx])
+    {
+      cmdVel.publish(wheel_power);
+    }
+    else
+    {
+      return;
+    }
+    `;
 
     // return the code
     return code;
   };
 
+
 ///////// DISTANCE DATA ///////////////////////////////////////////////////
-// Only use this block once per program
 Blockly.Blocks['getdistancedata'] = {
     init: function() {
       this.appendDummyInput()
@@ -539,6 +538,7 @@ Blockly.Blocks['getdistancedata'] = {
     `
     __DISTANCE__
     `;
-    // TODO: Change ORDER_NONE to the correct strength.
+
+    // Return code and order
     return [code, Blockly.JavaScript.ORDER_NONE];
   };
